babef4e52c05ed6d4bf610564e3c74eda69300c3
Mich <65708357+mich360@users.noreply.github.com>
2023-06-27 13:26:33 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className={`mb-3 text-2xl font-semibold`}>Nex.jsもReactのフレームワーク・Vercelへのデプロイ</h1>
       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>my-appという名前で編集しました</p>
-      {<a id="about" href="https://react-lottie-demo-rho.vercel.app/" target="_blank" rel="noopener noreferrer">
-     クリックしてこちらもご覧ください</a>}
+      {<a href="https://react-lottie-demo-rho.vercel.app/" target="_blank" rel="noopener noreferrer">クリックしてこちらもご覧ください</a>}
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
