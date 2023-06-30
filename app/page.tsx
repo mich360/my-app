fbd3f67bc395151cf6bfd85785bd31b6c3c72037
Mich <65708357+mich360@users.noreply.github.com>
2023-06-30 14:38:33 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,23 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className={`mb-3 text-2xl font-semibold`}>Nex.jsもReactのフレームワーク・Vercelへのデプロイ</h1>
       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>my-appという名前で編集しました</p>
+       <a
+          href="./about"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-1xl font-lighter`}>
+          About Page{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          Aboutページ編集して&nbsp;アニメーション&emsp;コンポーネントの追加ご覧ください
+          </p>
+        </a>
+      {/* 既存のコンテンツ */}
       
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
