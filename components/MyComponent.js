bd74c7c1401a3ab1ae558948d57277a855923775
Mich <65708357+mich360@users.noreply.github.com>
2023-07-13 20:16:25 +0900

    Update MyComponent.js

--- components/MyComponent.js
+++ components/MyComponent.js
@@ -3,9 +3,9 @@ import React from 'react';
 export function MyComponent({ data }) {
   return (
     <div>
-      <h2>My Component</h2>
-      <p>{data}</p>
-    </div>
+    <h2>My Component</h2>
+    <p>{data.example}</p>
+  </div>
   );
 }
 
